c8ed57b09234bbc9d9432f276c9001d4e15fd195
CigdemH <hkz34ing@gmail.com>
2023-03-30 15:18:24 +0300

    2gun2

--- src/G02_GitHub_ilkDers.java
+++ src/G02_GitHub_ilkDers.java
@@ -1,2 +1,6 @@
 public class G02_GitHub_ilkDers {
+    public static void main(String[] args) {
+        System.out.println("merhaba ben geldim");
+
+    }
 }
--- src/Gun3_b.java
+++ src/Gun3_b.java
@@ -1,6 +1,7 @@
 public class Gun3_b {
     public static void main(String[] args) {
         System.out.println("3.gun ancak oldu");
+        System.out.println("merhaba ben dunyali");
 
     }
 }
